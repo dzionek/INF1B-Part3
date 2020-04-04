2a5e51ab889a0d5a29569b232236b424a81d5677
dzionek <vburtoch@gmail.com>
2020-04-04 20:17:31 +0200

    Task 6 documented.

--- project/src/RemoveCmd.java
+++ project/src/RemoveCmd.java
@@ -1,36 +1,66 @@
-import java.awt.print.Book;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Remove command, which deletes entries from a library with a certain title or author.
+ */
 public class RemoveCmd extends LibraryCommand {
 
-    private static final String AUTHOR_ARG = "AUTHOR ";
-    private static final String TITLE_ARG = "TITLE ";
+    /** Command argument which says that we want to remove a book of a certain author. */
+    private static final String AUTHOR_ARG = "AUTHOR";
+    /** Command argument which says that we want to remove a book of a certain title. */
+    private static final String TITLE_ARG = "TITLE";
+    /** Gap between two arguments. */
+    private static final String PADDING = " ";
+    /** Message displayed after removing books by an author. */
+    private static final String REMOVE_AUTHOR_MESSAGE = " books removed for author: ";
+    /** Message displayed if there is no book with a given title. */
+    private static final String REMOVE_TITLE_NOT_FOUND_MESSAGE = ": not found.";
+    /** Message displayed after successfully removing books by a title. */
+    private static final String REMOVE_TITLE_SUCCESSFULLY = ": removed successfully.";
 
+    /** There are two modes of an instance, either {@value AUTHOR_ARG} or {@value TITLE_ARG}*/
     private String mode;
+    /** Full title or author name depending which {@link RemoveCmd#mode} we have.*/
     private String modeParameter;
 
+    /**
+     * @param argumentInput input is expected to be of the form
+     *                      "{@link RemoveCmd#mode} {@link RemoveCmd#modeParameter}"
+     * @throws IllegalArgumentException if given arguments are invalid.
+     * @throws NullPointerException if given arguments are null.
+     */
     public RemoveCmd(String argumentInput) {
         super(CommandType.REMOVE, argumentInput);
     }
 
+    /**
+     * Checks if the argument starts with {@value AUTHOR_ARG} or {@value TITLE_ARG}
+     * followed by one whitespace and non-blank author/title.
+     * @param argumentInput argument input for this command
+     * @return {@code true} if the argument is valid, otherwise {@code false}.
+     */
     @Override
     protected boolean parseArguments(String argumentInput) {
         Objects.requireNonNull(argumentInput, "Given input argument must not be null.");
 
-        if (argumentInput.startsWith(AUTHOR_ARG)) {
+        if (argumentInput.startsWith(AUTHOR_ARG + PADDING)) {
             mode = AUTHOR_ARG;
-        } else if (argumentInput.startsWith(TITLE_ARG)) {
+        } else if (argumentInput.startsWith(TITLE_ARG + PADDING)) {
             mode = TITLE_ARG;
         } else {
             return false;
         }
 
-        modeParameter = argumentInput.substring(mode.length());
+        modeParameter = argumentInput.substring(mode.length() + PADDING.length());
         return !modeParameter.isBlank();
     }
 
+    /**
+     * Executes the remove command and remove all books with a certain title/author.
+     * @param data book data to be considered for command execution.
+     */
     @Override
     public void execute(LibraryData data) {
         Objects.requireNonNull(data, "Library data must not be null.");
@@ -44,6 +74,10 @@ public class RemoveCmd extends LibraryCommand {
         }
     }
 
+    /**
+     * Remove a book with a given title, if possible.
+     * @param booksIterator iterator of a list of all books.
+     */
     private void removeTitle(Iterator<BookEntry> booksIterator) {
         boolean isRemoved = false;
         while (booksIterator.hasNext() && !isRemoved) {
@@ -58,14 +92,23 @@ public class RemoveCmd extends LibraryCommand {
         printRemoveTitle(isRemoved);
     }
 
+    /**
+     * Displays a message after removing a book by a title.
+     * @param isRemoved {@code true} if a book was removed,
+     *                  {@code false} if no book was removed.
+     */
     private void printRemoveTitle(boolean isRemoved) {
         if (isRemoved) {
-            System.out.println(modeParameter + ": removed successfully.");
+            System.out.println(modeParameter + REMOVE_TITLE_SUCCESSFULLY);
         } else {
-            System.out.println(modeParameter + ": not found.");
+            System.out.println(modeParameter + REMOVE_TITLE_NOT_FOUND_MESSAGE);
         }
     }
 
+    /**
+     * Remove a book with a given title, if possible.
+     * @param booksIterator iterator of a list of all books.
+     */
     private void removeAuthor(Iterator<BookEntry> booksIterator) {
         int numberOfRemoved = 0;
         while (booksIterator.hasNext()) {
@@ -82,7 +125,11 @@ public class RemoveCmd extends LibraryCommand {
         printRemoveAuthor(numberOfRemoved);
     }
 
+    /**
+     * Displays a message after removing books by an author.
+     * @param numberOfRemoved number of books that were removed.
+     */
     private void printRemoveAuthor(int numberOfRemoved) {
-        System.out.println(numberOfRemoved + " books removed for author: " + modeParameter);
+        System.out.println(numberOfRemoved + REMOVE_AUTHOR_MESSAGE + modeParameter);
     }
 }
